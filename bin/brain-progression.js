70f8b71c6c9a4de57a70cd7a682409504e412a90
Ledloy <d-os@inbox.ru>
2026-01-09 23:41:38 +0300

    Add brain-progression executable

---
+++ bin/brain-progression.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import { getUserName } from '../src/cli.js';
+import { runGame } from '../src/index.js';
+import { getQuestionAndAnswer as getProgressionQA } from '../src/games/progression.js';
+
+const name = getUserName();
+runGame(name, getProgressionQA, 'What number is missing in the progression?');
