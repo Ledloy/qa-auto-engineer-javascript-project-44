bec7c442d8261fcc2361284ec255d1e0da1de2a6
Ledloy <d-os@inbox.ru>
2026-01-11 21:45:23 +0300

    Fix bin/brain-games to show welcome message without args

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
+
 import { getGameName } from '../src/cli.js'
 
 const gameName = getGameName()
+
+if (!gameName) {
+  console.log('Welcome to the Brain Games!')
+  console.log('Please choose a game: even, calc, gcd, progression, prime')
+  process.exit(0)
+}
+
 switch (gameName) {
   case 'even':
     import('../src/games/even.js').then(m => m.default())
