a9f34c398b6ab33812c8bbbd874b268a507c23eb
Ledloy <d-os@inbox.ru>
2026-01-20 19:48:41 +0300

    Сохраняю мои правки

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export const getRandomInt = (min, max) => {
   const ceiledMin = Math.ceil(min)
   const flooredMax = Math.floor(max)
-  return Math.floor(Math.random() * (flooredMax- ceiledMin + 1)) + ceiledMin
+  return Math.floor(Math.random() * (flooredMax - ceiledMin + 1)) + ceiledMin
 }
